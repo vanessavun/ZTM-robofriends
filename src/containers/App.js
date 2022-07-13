ded65a754ba82552fc81bd192135f501db507797
Vanessa Vun <vanessavun@gmail.com>
2022-07-13 10:37:17 -0700

    Add error boundaries

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll'
+import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 class App extends React.Component {
@@ -35,7 +36,9 @@ class App extends React.Component {
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
+                        <ErrorBoundary>
                         <CardList robots={filteredRobot} />
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             );   
